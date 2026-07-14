77c1a8cf2ee992d2f71338a78c5a1801fa477a63
xulei-shl <xl_0527@163.com>
2026-07-14 09:04:09 +0800

    脚本文件清理

--- scripts/check-users.ts
+++
@@ -1,5 +0,0 @@
-import database from 'better-sqlite3';
-const db = new database('./data/rss-tracker.db');
-const users = db.prepare('SELECT username, role FROM users').all();
-console.table(users);
-db.close();
--- scripts/test-force-schedule.ts
+++
@@ -1,6 +0,0 @@
-import 'dotenv/config';
-
-const forceOnSchedule = process.env.RSS_FETCH_FORCE_ON_SCHEDULE === 'true';
-console.log('RSS_FETCH_FORCE_ON_SCHEDULE:', process.env.RSS_FETCH_FORCE_ON_SCHEDULE);
-console.log('forceOnSchedule (parsed):', forceOnSchedule);
-console.log('Type:', typeof forceOnSchedule);
